57c16bb6e671eafaac124a3823c4b8f40fcca4c3
HagenSV22 <HagenSV22@gcc.edu>
2024-09-23 15:10:25 -0400

    Make APIRequest basis of all request handlers

--- library/webgame/api/APIRequest.java
+++ library/webgame/api/APIRequest.java
@@ -1,18 +1,32 @@
 package library.webgame.api;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
 
 import library.webgame.WebGame;
 
-public class APIRequest {
+public abstract class APIRequest implements HttpHandler {
     
     private static final Pattern namePattern = Pattern.compile( "name=([^;]*)" );
     private static final Pattern sessionPattern = Pattern.compile( "session=(\\w*);?" );
-
     
-    public static int validate(WebGame game, HttpExchange exchange){
+    protected final WebGame game;
+
+    public APIRequest(String path, WebGame game){
+        this.game = game;
+
+        game.gameServer.createContext(path,this);
+    }
+    
+    public int validate(HttpExchange exchange){
         //Get cookies sent with the request
         String cookies = exchange.getRequestHeaders().getFirst("Cookie");
         cookies = cookies == null ? "" : cookies;
@@ -27,5 +41,23 @@ public class APIRequest {
         int playerId = game.playerManager.getPlayers().indexOf(playerName);
 
         return sessionId.equals(game.GAME_ID) ? playerId : -1;
+    }
+
+    public static void sendString( HttpExchange exchange, int rCode, String s ) throws IOException {
+        exchange.sendResponseHeaders( rCode, s.length() );
+
+        OutputStream out = exchange.getResponseBody();
+
+        out.write( s.getBytes() );
+        out.close();
+    }
+
+    public static void sendFile( HttpExchange exchange, int rCode, File f ) throws IOException {
+        exchange.sendResponseHeaders( rCode, f.length() );
+
+        OutputStream out = exchange.getResponseBody();
+
+        Files.copy( f.toPath(), out );
+        out.close();   
     }
 }
