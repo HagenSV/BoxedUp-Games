a97b19bf1e8362ba72f1faf9a48cc38f3d2d71e5
HagenSV22 <HagenSV22@gcc.edu>
2024-09-23 15:23:38 -0400

    increased visibility of member variables

--- library/webgame/PlayerViewManager.java
+++ library/webgame/PlayerViewManager.java
@@ -3,20 +3,12 @@ package library.webgame;
 import com.sun.net.httpserver.HttpExchange;
 public abstract class PlayerViewManager {
     
-    private GameStateManager gameStateManager;
-    private PlayerManager playerManager;
+    protected final GameStateManager gameStateManager;
+    protected final PlayerManager playerManager;
 
     public PlayerViewManager(GameStateManager gsm, PlayerManager pm){
         this.gameStateManager = gsm;
         this.playerManager = pm;
-    }
-
-    public GameStateManager getGameStateManager(){
-        return gameStateManager;
-    }
-
-    public PlayerManager getPlayerManager(){
-        return playerManager;
     }
 
     public abstract void sendScreen(HttpExchange exchange, int playerId);
