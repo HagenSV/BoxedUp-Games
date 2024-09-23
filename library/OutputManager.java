ac04b6d680920acc61377d68cedaedc47ed89066
HagenSV22 <HagenSV22@gcc.edu>
2024-09-23 08:02:38 -0400

    Fixed getPlayerManager() method

--- library/OutputManager.java
+++ library/OutputManager.java
@@ -9,7 +9,7 @@ public abstract class OutputManager {
     }
 
     protected PlayerManager getPlayerManager(){
-        return this.getPlayerManager();
+        return playerManager;
     }
 
     /**
