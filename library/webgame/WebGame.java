3bfed890b60760ebac0089c76d28d82756ef4624
HagenSV22 <HagenSV22@gcc.edu>
2024-09-23 15:09:46 -0400

    Refactor webgame

---
+++ library/webgame/WebGame.java
@@ -0,0 +1,39 @@
+package library.webgame;
+
+import library.Server;
+
+import java.io.IOException;
+
+
+public abstract class WebGame {
+
+    public final String GAME_ID;
+
+    public final PlayerManager playerManager;
+    public final GameStateManager gameStateManager;
+    public final PlayerViewManager playerViewManager;
+    public final Server gameServer;
+
+    private boolean gameOver;
+
+    public WebGame(PlayerManager pm, GameStateManager gsm, PlayerViewManager pv) throws IOException {
+        GAME_ID = ""+(int)(Math.random()*1_000_000);
+        playerManager = pm;
+        gameStateManager = gsm;
+        playerViewManager = pv;
+
+        gameOver = false;
+        gameServer = new Server();
+    }
+
+    public abstract void start();
+
+    public boolean canStart(){ return playerManager.hasMin(); }
+
+    public boolean isOver(){ return gameOver; }
+
+    public void endGame(){
+        gameOver = true;
+        gameServer.close();
+    }
+}
