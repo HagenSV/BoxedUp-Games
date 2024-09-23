5700dd31c8a608fa46d9cebfe521283bad992027
HagenSV22 <HagenSV22@gcc.edu>
2024-09-23 13:22:35 -0400

    Create class to manage client screens

---
+++ library/PlayerViewManager.java
@@ -0,0 +1,22 @@
+package library;
+
+public abstract class PlayerViewManager {
+    
+    private GameStateManager gameStateManager;
+    private PlayerManager playerManager;
+
+    public PlayerViewManager(GameStateManager gsm, PlayerManager pm){
+        this.gameStateManager = gsm;
+        this.playerManager = pm;
+    }
+
+    public GameStateManager getGameStateManager(){
+        return gameStateManager;
+    }
+
+    public PlayerManager getPlayerManager(){
+        return playerManager;
+    }
+
+    public abstract void sendScreen(int playerId, int gameState);
+}
