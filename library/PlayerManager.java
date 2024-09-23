f3fd87f26f3e8260c51b1012636e90670f420d23
HagenSV22 <HagenSV22@gcc.edu>
2024-09-23 08:01:15 -0400

    Abstract class to hande player data for all games

---
+++ library/PlayerManager.java
@@ -0,0 +1,76 @@
+package library;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class PlayerManager {
+
+    private final int MIN_PLAYERS;
+    private final int MAX_PLAYERS;
+
+    private List<String> playerList;
+    private List<Integer> playerPhase;
+
+    /**
+     * Creates a new player manager
+     * @param minPlayers the minimum number of players required for the game to start
+     * @param maxPlayers the maximum number of players the game can have
+     */
+    public PlayerManager(int minPlayers, int maxPlayers){
+        this.MIN_PLAYERS = minPlayers;
+        this.MAX_PLAYERS = maxPlayers;
+
+        playerList = new ArrayList<>(MAX_PLAYERS);
+        playerPhase = new ArrayList<>(MAX_PLAYERS);
+    }
+
+    /**
+     * Attempts to add a player to the game
+     * @param playerName the desired name of the player to add
+     * @return the resolved name of the player or null if the player could not be added
+     */
+    public String addPlayer(String playerName){
+        if ( playerList.size() == MAX_PLAYERS ) {
+            return null;
+        }
+
+        if ( playerList.contains( playerName ) ) {
+            for ( int i = 1; i < MAX_PLAYERS; i++ ) {
+                if ( !playerList.contains( playerName+" "+i ) ) {
+                    playerList.add( playerName+" "+i );
+                    return playerName+" "+i;
+                }
+            }
+        }
+
+        playerList.add( playerName );
+
+        return playerName;
+    }
+    
+    /**
+     * Gets player data
+     * @return an object storing all players' data
+     */
+    public abstract Object getPlayerData();
+
+    /**
+     * 
+     * @param playerId
+     * @return the phase id of the player
+     */
+    public int getPlayerPhase(int playerId){
+        return playerPhase.get(playerId);
+    }
+
+    public boolean hasMin(){ return playerList.size() >= MIN_PLAYERS; }
+
+    /**
+     * Sets the phase of a player
+     * @param playerId
+     * @param phaseId
+     */
+    public void setPlayerPhase(int playerId, int phaseId){
+        playerPhase.set(playerId, phaseId);
+    }
+}
