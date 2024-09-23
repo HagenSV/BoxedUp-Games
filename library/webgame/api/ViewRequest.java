e6dd704c05a76b8980d2662ba6f40de21eb4433a
HagenSV22 <HagenSV22@gcc.edu>
2024-09-23 15:11:00 -0400

    API handler for player view

---
+++ library/webgame/api/ViewRequest.java
@@ -0,0 +1,29 @@
+package library.webgame.api;
+
+import library.webgame.WebGame;
+
+import java.io.IOException;
+import com.sun.net.httpserver.HttpExchange;
+
+public class ViewRequest extends APIRequest {
+
+    public static final String PATH = "/game-status/";
+
+    public ViewRequest(WebGame game){
+        super(PATH,game);
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+    
+        int playerId = validate(exchange);
+
+        if (playerId == -1){
+            APIRequest.sendString(exchange, 403, "Forbidden");
+            return;
+        }
+
+        game.playerViewManager.sendScreen(0, game.gameStateManager.getPhase());
+    }
+    
+}
