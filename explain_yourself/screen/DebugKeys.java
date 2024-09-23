3a5c3bfedde4b96164f2aee7fa8c7bf505b26000
HagenSV22 <HagenSV22@gcc.edu>
2024-09-23 18:22:56 -0400

    Fixed issues from refactor

--- explain_yourself/screen/DebugKeys.java
+++ explain_yourself/screen/DebugKeys.java
@@ -5,7 +5,7 @@ import java.awt.event.KeyEvent;
 
 import explain_yourself.ExplainGame;
 
-import static explain_yourself.GameConfigs.*;
+import static explain_yourself.ExplainGameConfigs.*;
 
 public class DebugKeys extends KeyAdapter {
 
@@ -20,7 +20,7 @@ public class DebugKeys extends KeyAdapter {
         switch (e.getKeyCode()) {
             
             case KeyEvent.VK_1:
-                game.setPhase(JOIN_PHASE);
+                game.gameStateManager.setPhase(JOIN_PHASE);
                 break;
         
             case KeyEvent.VK_2:
@@ -28,24 +28,24 @@ public class DebugKeys extends KeyAdapter {
                 break;
 
             case KeyEvent.VK_3:
-                game.setPhase(CARD_INTRO_PHASE);
+                game.gameStateManager.setPhase(CARD_INTRO_PHASE);
                 break;
 
             case KeyEvent.VK_4:
-                game.setPhase(VOTE_PHASE);
+                game.gameStateManager.setPhase(VOTE_PHASE);
                 break;
 
             case KeyEvent.VK_5:
-                game.setPhase(VOTE_RESULTS_PHASE);
+                game.gameStateManager.setPhase(VOTE_RESULTS_PHASE);
                 break;
 
             case KeyEvent.VK_6:
-                game.setPhase(GAME_OVER);
+                game.gameStateManager.setPhase(GAME_OVER);
                 break;
 
 
             case KeyEvent.VK_J:
-                game.addPlayer("Bot");
+                game.playerManager.addPlayer("Bot");
                 break;
 
             default:
