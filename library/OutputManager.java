f875c05fbb2fbae083e2c774310ab190199fd228
HagenSV22 <HagenSV22@gcc.edu>
2024-09-23 08:01:30 -0400

    Abstract class to manage game output

---
+++ library/OutputManager.java
@@ -0,0 +1,20 @@
+package library;
+
+public abstract class OutputManager {
+
+    private PlayerManager playerManager;
+
+    public OutputManager(PlayerManager playerManager){
+        this.playerManager = playerManager;
+    }
+
+    protected PlayerManager getPlayerManager(){
+        return this.getPlayerManager();
+    }
+
+    /**
+     * Called whenever the game phase changes
+     * @param screenId the id of the new phase
+     */
+    public abstract void renderScreen(int screenId);
+}
