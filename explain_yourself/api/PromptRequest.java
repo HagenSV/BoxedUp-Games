6abc25fd3133595a29cea69038b968fb0ce24469
HagenSV22 <HagenSV22@gcc.edu>
2024-09-24 09:43:19 -0400

    Fix conditions

--- explain_yourself/api/PromptRequest.java
+++ explain_yourself/api/PromptRequest.java
@@ -1,5 +1,7 @@
 package explain_yourself.api;
 
+import static explain_yourself.ExplainGameConfigs.PROMPT_PHASE;
+
 import java.io.IOException;
 
 import com.sun.net.httpserver.HttpExchange;
@@ -22,11 +24,11 @@ public class PromptRequest extends APIRequest {
     public void handle(HttpExchange exchange) throws IOException {
         int playerId = validate(exchange);
 
-        if (playerId == -1){
-            sendResponse(exchange, 403, "Forbidden");
+        if (playerId == -1 || game.gameStateManager.getPhase() != PROMPT_PHASE ){
+            send401AccessDenied(exchange);
             return;
         }
-        
+
         sendResponse( exchange, 200, gameData.getPrompt( playerId, gameData.getResponseCount(playerId) ) );
     }
     
