b19f83103727ad6bd2ddadaeca6b2780d5cd6be3
HagenSV22 <HagenSV22@gcc.edu>
2024-09-24 09:43:48 -0400

    Disable old test code

--- main/Main.java
+++ main/Main.java
@@ -1,9 +1,4 @@
 package main;
-import explain_yourself.ExplainGame;
-import library.webgame.WebGame;
-
-import java.net.InetAddress;
-import java.util.Scanner;
 
 public class Main {
 
@@ -15,39 +10,35 @@ public class Main {
 
     }
 
-    /**
-     * @deprecated
-     * Due to changes with the ExplainGame class, this method will no longer work
-     */
-    @Deprecated
+    
     public static void consoleGame(){
 
-        Scanner s = new Scanner(System.in);
+        // Scanner s = new Scanner(System.in);
 
-        try {
+        // try {
 
-            WebGame game = new ExplainGame();
+        //     WebGame game = new ExplainGame();
             
-            System.out.println("Server hosted on "+InetAddress.getLocalHost().getHostAddress());
+        //     System.out.println("Server hosted on "+InetAddress.getLocalHost().getHostAddress());
 
-            System.out.print("Begin? ");
-            s.nextLine();
-            while (!game.canStart()){
-                System.out.println("Not enough players.");
-                System.out.print("Begin? ");
-                s.nextLine();
-            }
+        //     System.out.print("Begin? ");
+        //     s.nextLine();
+        //     while (!game.canStart()){
+        //         System.out.println("Not enough players.");
+        //         System.out.print("Begin? ");
+        //         s.nextLine();
+        //     }
             
-            System.out.println("Starting game...");
-            game.start();
-            while (!game.isOver()){}
-            System.out.println("Thanks for playing!");
+        //     System.out.println("Starting game...");
+        //     game.start();
+        //     while (!game.isOver()){}
+        //     System.out.println("Thanks for playing!");
 
-        } catch (Exception e){
-            e.printStackTrace();
-            System.out.println("Failed to initialize server");
-        }
+        // } catch (Exception e){
+        //     e.printStackTrace();
+        //     System.out.println("Failed to initialize server");
+        // }
 
-        s.close();
+        // s.close();
     }
 }
