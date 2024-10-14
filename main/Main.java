f14a0e328da1781e01a428cdedfacc3600c03db2
HagenSV22 <HagenSV22@gcc.edu>
2024-10-14 09:40:23 -0400

    Start logging whenever program starts

--- main/Main.java
+++ main/Main.java
@@ -1,9 +1,11 @@
 package main;
 
+import library.OutputLog;
 public class Main {
 
     public static final int THREAD_POOL_SIZE = 10;
     public static void main(String[] args) {
+        OutputLog.getInstance().log("Program Started");
         MenuManager.getInstance();
     }
 }
